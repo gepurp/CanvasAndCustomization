c9f6b4de129b97c577d6195516e92af09107a337
pkonstantinov <pkonstantinov@tradingview.com>
2021-08-22 16:27:41 +0300

    Define canvas and brush

--- app/src/main/java/com/octo/canvasandcustomization/DrawingView.kt
+++ app/src/main/java/com/octo/canvasandcustomization/DrawingView.kt
@@ -3,6 +3,7 @@ package com.octo.canvasandcustomization
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(
@@ -33,6 +34,46 @@ class DrawingView(
         canvasPaint = Paint(Paint.DITHER_FLAG)
         brushSize = 20.toFloat()
     }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint)
+        if (!drawPath!!.isEmpty) {
+            drawPaint!!.strokeWidth = drawPath!!.brushThickness
+            drawPaint!!.color = drawPath!!.color
+            canvas.drawPath(drawPath!!, drawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawPath!!.color = color
+                drawPath!!.brushThickness = brushSize
+                drawPath!!.reset()
+                drawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                drawPath!!.lineTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                drawPath = CustomPath(color, brushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
 
     internal inner class CustomPath(
         var color: Int,
