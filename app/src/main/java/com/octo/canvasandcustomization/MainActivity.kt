32d8572f568e1f8e23a1869cd9d5a3ad0139340e
pkonstantinov <pkonstantinov@tradingview.com>
2021-08-29 12:44:25 +0300

    Handle storage permission

--- app/src/main/java/com/octo/canvasandcustomization/MainActivity.kt
+++ app/src/main/java/com/octo/canvasandcustomization/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.octo.canvasandcustomization
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.octo.canvasandcustomization.databinding.ActivityMainBinding
 import dev.sasikanth.colorsheet.ColorSheet
 
@@ -11,6 +16,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var colors: IntArray
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,13 @@ class MainActivity : AppCompatActivity() {
             }
             ibSelectBrushColor.setOnClickListener {
                 showColorPicker()
+            }
+            ibPickImageFromGallery.setOnClickListener {
+                if (isStorageAccessAllowed()) {
+                    pickImageFromGallery()
+                } else {
+                    requestStoragePermission()
+                }
             }
         }
     }
@@ -59,5 +75,68 @@ class MainActivity : AppCompatActivity() {
                 binding.dvDrawing.setBrushColor(color)
             })
             .show(supportFragmentManager)
+    }
+
+    private fun requestStoragePermission() {
+        if (
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ).toString()
+            )
+        ) {
+            Toast.makeText(
+                this,
+                "Need permission to add a Background",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ),
+            STORAGE_PERMISSION_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Now you can add background",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Oops... Now you cannot add background...",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun isStorageAccessAllowed(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun pickImageFromGallery() {
+        // TODO: Needs to implement
     }
 }
